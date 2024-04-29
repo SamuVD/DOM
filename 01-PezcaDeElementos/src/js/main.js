bb813d1a28d0c68830d6227b781efa13e93d85e5
SamuVD <samuvv2905@gmail.com>
2024-04-29 09:23:08 -0500

    Vimos como pezcar elementos desde el js

--- 01-PezcaDeElementos/src/js/main.js
+++ 01-PezcaDeElementos/src/js/main.js
@@ -1,7 +1,7 @@
 let first = document.getElementById("btn-first") //call an element with id
 let second = document.getElementsByClassName("btn-second") //call an element with class name
 let third = document.getElementsByName("btn-third") //call an element with
-let fourth = document.getElementsByTagName("button")
+let fourth = document.getElementsByTagName("button") // call an element with tag name
 
 
 // console.log(first)
@@ -10,6 +10,8 @@ let fourth = document.getElementsByTagName("button")
 // console.log(fourth[3])
 
 // Lamar 4 botones desde html utilizando solo querySelector y querySelectorAll
+console.groupCollapsed("element one")
+
 let one = document.querySelector("#btn-one")
 let two = document.querySelector(".btn-two")
 let three = document.querySelector("[name='btn-three']")
@@ -19,3 +21,5 @@ console.log(one)
 console.log(two)
 console.log(three)
 console.log(four[7])
+
+console.groupEnd()
