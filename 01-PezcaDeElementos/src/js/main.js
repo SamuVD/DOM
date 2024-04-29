01e366c454ea11900a648cb939b398fec163d238
SamuVD <samuvv2905@gmail.com>
2024-04-29 08:58:16 -0500

    Viendo las formas de llamar botones

---
+++ 01-PezcaDeElementos/src/js/main.js
@@ -0,0 +1,21 @@
+let first = document.getElementById("btn-first") //call an element with id
+let second = document.getElementsByClassName("btn-second") //call an element with class name
+let third = document.getElementsByName("btn-third") //call an element with
+let fourth = document.getElementsByTagName("button")
+
+
+// console.log(first)
+// console.log(second[0])
+// console.log(third[0])
+// console.log(fourth[3])
+
+// Lamar 4 botones desde html utilizando solo querySelector y querySelectorAll
+let one = document.querySelector("#btn-one")
+let two = document.querySelector(".btn-two")
+let three = document.querySelector("[name='btn-three']")
+let four = document.querySelectorAll("button")
+
+console.log(one)
+console.log(two)
+console.log(three)
+console.log(four[7])
